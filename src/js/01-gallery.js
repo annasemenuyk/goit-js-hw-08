7787b07ae1a0a7cbf50e83945d2108470737d76b
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-23 00:55:04 +0200

    1 part

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,36 +1,13 @@
 // Описан в документации
-import SimpleLightbox from "simplelightbox"
+import SimpleLightbox from 'simplelightbox'
 // Дополнительный импорт стилей
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import pictureCardTmp from '../templates/gallery-tpl'
 // Change code below this line
-
-console.log(galleryItems);
-const item =
-`<a class="gallery__item" href="large-image.jpg">
-<img class="gallery__image" src="small-image.jpg" alt="Image description" />
-</a> `
-function createItemCards (arrey){
-    return arrey.map((el)=>{
-        const {description, original, preview} = el
-        //console.log (el)
-            return`
-        <a class="gallery__item" href='${original}'>
-              <img class="gallery__image" src='${preview}' alt='${description}' />
-        </a>`
-  })
-   .join('')
-}
-const markup = createItemCards (galleryItems);
-// console.log (markup);
-const list = document.querySelector('.gallery');
-list.insertAdjacentHTML("afterbegin", markup);
-// Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery. 
-// Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
-// Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt.
-// Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
-
+const list = document.querySelector('.gallery')
+list.innerHTML = pictureCardTmp(galleryItems);
 const captionOptions = {
     captionSelector: 'img',//установить элемент, в котором находится заголовок. Установите для самого A-Tag значение «self» или используйте обратный вызов, который возвращает элемент.
     captionType: 'attr',//как получить подпись. Вы можете выбирать между attr, data или text
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
-feedbackForm: document.querySelector('.feedback-form')
+
+import throttle from 'lodash.throttle'
+
+const feedbackForm = document.querySelector('.feedback-form')
+
+if (localStorage.getItem('feedback-form-state')) {
+    feedbackForm.elements.email.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+}
+
+if (localStorage.getItem('feedback-form-state')) {
+    feedbackForm.elements.message.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
+}
+
+feedbackForm.addEventListener('input', throttle(onFormInputChange, 500));
+feedbackForm.addEventListener('submit', () => {localStorage.removeItem('feedback-form-state')})
+
+let inputsValues = {
+        email: '',
+        message: '',
+    }
+
+function onFormInputChange(e) {
+   
+    if (e.currentTarget.elements.email.value) {
+        inputsValues.email = e.currentTarget.elements.email.value;
+    }
+
+    if (e.currentTarget.elements.message.value) {
+        inputsValues.message = e.currentTarget.elements.message.value;
+    }
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(inputsValues))
+}
